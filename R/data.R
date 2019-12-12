3cf8958bfc90d1e47078f23d706526dfcb9004d6
dicook <visnut@gmail.com>
2019-12-12 15:24:29 +1100

    data description

--- R/data.R
+++ R/data.R
@@ -1,1 +1,23 @@
-# Description of data files
+#` Student data from 2000-2018 PISA OECD data
+#`
+#` A dataset containing scores and other information
+#` from the triennial testing of 15 year olds around
+#` the globe. Original data available from
+#`  \url{https://www.oecd.org/pisa/data/}.
+#`
+#` @format A tibble of the following variables
+#` \itemize{
+#`   \item student_id
+#`   \item stu_wgt
+#`   \item country
+#`   \item school_id
+#`   \item mother_educ
+#`   \item father_educ
+#`   \item gender
+#`   \item computer
+#`   \item internet
+#`   \item math
+#`   \item science
+#`   \item read
+#` }
+"student"
