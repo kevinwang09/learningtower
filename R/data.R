a80a34f19d7e2863513575d660f36c3223f2da5c
dicook <visnut@gmail.com>
2019-12-13 16:26:13 +1100

    got data man files working

--- R/data.R
+++ R/data.R
@@ -1,75 +1,75 @@
-#` Student data from 2000-2018 PISA OECD data
-#`
-#` A dataset containing scores and other information
-#` from the triennial testing of 15 year olds around
-#` the globe. Original data available from
-#`  \url{https://www.oecd.org/pisa/data/}.
-#`
-#` @format A tibble of the following variables
-#` \itemize{
-#`   \item country_iso3c Country 3 character code
-#`   \item school_id The unique school identification number
-#`   \item student_id The student identification number
-#`   \item mother_educ Highest level of mother's education - 1 to 5
-#`   \item father_educ Highest level of father's education - 1 to 5
-#`   \item gender recorded gender - 1 female or 2 male or missing
-#`   \item computer Possession of computer - 1 yes, 2 no or missing
-#`   \item internet Access to internet - 1 yes, 2 no or missing
-#`   \item math Simulated score in mathematics
-#`   \item read Simulated score in reading
-#`   \item science Simulated score in science
-#`   \item stu_wgt The final survey weight score for the student score
-#`   \item desk Possession of  desk to study at - 1 yes, 2 no or missing
-#`   \item room Possession of a room of your own - 1 yes, 2 no or missing
-#`   \item dishwasher Possession of a dishwasher - 1 yes, 2 no or missing
-#`   \item television Number of televisions - 1 yes, 2 no or missing
-#`   \item computer_n Number of computers - 1 yes, 2 no or missing
-#`   \item car Number of cars - 1 yes, 2 no or missing
-#`   \item book Number of books - 1 yes, 2 no or missing
-#`   \item wealth Family wealth
-#`   \item escs Index of economic, social and cultural status
-#` }
-#` @docType data
-#` @name student
+#' Student data from 2000-2018 PISA OECD data
+#'
+#' A dataset containing scores and other information
+#' from the triennial testing of 15 year olds around
+#' the globe. Original data available from
+#'  \url{https://www.oecd.org/pisa/data/}.
+#'
+#' @format A tibble of the following variables
+#' \itemize{
+#'     \item country_iso3c Country 3 character code
+#'     \item school_id The unique school identification number
+#'     \item student_id The student identification number
+#'     \item mother_educ Highest level of mother's education - 1 to 5
+#'     \item father_educ Highest level of father's education - 1 to 5
+#'     \item gender recorded gender - 1 female or 2 male or missing
+#'     \item computer Possession of computer - 1 yes, 2 no or missing
+#'     \item internet Access to internet - 1 yes, 2 no or missing
+#'     \item math Simulated score in mathematics
+#'     \item read Simulated score in reading
+#'     \item science Simulated score in science
+#'     \item stu_wgt The final survey weight score for the student score
+#'     \item desk Possession of  desk to study at - 1 yes, 2 no or missing
+#'     \item room Possession of a room of your own - 1 yes, 2 no or missing
+#'     \item dishwasher Possession of a dishwasher - 1 yes, 2 no or missing
+#'     \item television Number of televisions - 1 yes, 2 no or missing
+#'     \item computer_n Number of computers - 1 yes, 2 no or missing
+#'     \item car Number of cars - 1 yes, 2 no or missing
+#'     \item book Number of books - 1 yes, 2 no or missing
+#'     \item wealth Family wealth
+#'     \item escs Index of economic, social and cultural status
+#'     }
+#' @docType data
+#' @name student
 NULL
 
-#` School data from 2000-2018 PISA OECD data
-#`
-#` A dataset containing school weight and other information
-#` from the triennial testing of 15 year olds around
-#` the globe. Original data available from
-#`  \url{https://www.oecd.org/pisa/data/}.
-#`
-#` @format A tibble of the following variables
-#` \itemize{
-#`   \item student_id The student identification number
-#`   \item stu_wgt The final survey weight score for the student score
-#`   \item country_iso3c Country 3 character code
-#`   \item school_id The unique school identification number
-#`   \item funding_gov Percentage of total funding for school year from: Government
-#`   \item funding_fees Percentage of total funding for school year from: Student fees or school charges paid by parents
-#`   \item funding_donations Percentage of total funding for school year from: Benefactors, donations, bequests, sponsorships, parent fundraising
-#`   \item enrollment_boys Number of boys
-#`   \item enrollment_girls Number of girls
-#`   \item student_teacher_ratio Student-Teacher ratio
-#`   \item public_private Public or private school
-#`   \item staff_shortage The unique school identification number
-#`   \item sch_wgt Adjusted school base weight
-#`   \item school_size The school size
-#` }
-#` @docType data
-#` @name school
+#' School data from 2000-2018 PISA OECD data
+#'
+#' A dataset containing school weight and other information
+#' from the triennial testing of 15 year olds around
+#' the globe. Original data available from
+#'  \url{https://www.oecd.org/pisa/data/}.
+#'
+#' @format A tibble of the following variables
+#' \itemize{
+#'     \item student_id The student identification number
+#'     \item stu_wgt The final survey weight score for the student score
+#'     \item country_iso3c Country 3 character code
+#'     \item school_id The unique school identification number
+#'     \item funding_gov Percentage of total funding for school year from: Government
+#'     \item funding_fees Percentage of total funding for school year from: Student fees or school charges paid by parents
+#'     \item funding_donations Percentage of total funding for school year from: Benefactors, donations, bequests, sponsorships, parent fundraising
+#'     \item enrollment_boys Number of boys
+#'     \item enrollment_girls Number of girls
+#'     \item student_teacher_ratio Student-Teacher ratio
+#'     \item public_private Public or private school
+#'     \item staff_shortage The unique school identification number
+#'     \item sch_wgt Adjusted school base weight
+#'     \item school_size The school size
+#'     }
+#' @docType data
+#' @name school
 NULL
 
-#` Country Mapping Data PISA OECD data
-#`
-#` A dataset containing mapping of the country ISO code to the country name.
-#` @format A tibble of the following variables
-#`
-#` \itemize{
-#`   \item country_iso3c Country 3 character code
-#`   \item country_name Country name
-#` }
-#` @docType data
-#` @name countrycode
+#' Country iso3c and name mapping for PISA OECD countries
+#'
+#' A dataset containing mapping of the country ISO code to the country name.
+#'
+#' @format A tibble of the following variables
+#' \itemize{
+#'     \item country_iso3c Country 3 character code
+#'     \item country_name Country name
+#'     }
+#' @docType data
+#' @name countrycode
 NULL
