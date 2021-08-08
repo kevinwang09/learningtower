174a811262f882297a4d2732e3b9bb1ca44a6f95
Priya Ravindra Dingorkar <pdin0008@student.monash.edu>
2021-08-09 00:35:15 +1000

    Added more components to test that

--- tests/testthat/test-test-load.R
+++ tests/testthat/test-test-load.R
@@ -5,3 +5,9 @@ test_that("load multiple yeas", {
 test_that("load non-existing years", {
   expect_error(load_student("2001"))
 })
+
+
+test_that("test for load_student class functions",{
+  expect_s3_class(object = load_student(year = 2000),
+                  class = c("tbl_df", "tbl", "data.frame"))
+})
