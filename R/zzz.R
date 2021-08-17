3790066fb2a19904e8768eb71602802b75eb57e8
Priya Ravindra Dingorkar <pdin0008@student.monash.edu>
2021-08-18 02:58:17 +1000

    Added student_country vignette

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to LearningTower to access the entire student data for each year run the following code.
-Repeat the same code similarly for the desired years.
-student_data_2000 <- load_student(2000)")
+  packageStartupMessage("The learningtower package provides data from OECD PISA database.
+For size reasons only a small subset is provided in the package.
+Use the function load_student() to access the full data.")
 }
