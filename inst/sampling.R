d1a7230e969bfc50486eabccc2f674123bd4aaa0
Guan Ru Chen <gche0054@student.monash.edu>
2024-09-17 23:11:33 +1000

    Add seed for reproducibility

--- inst/sampling.R
+++ inst/sampling.R
@@ -1,12 +1,20 @@
 # This is the code to generate the subset of full student dataset to go into package
 
 library(here)
+library(haven)
+library(skimr)
 library(tidyverse)
 
+# Set seed for reproducibility
+set.seed(2022)
+
+# Load full dataset
 student_2022 <- readRDS(here("student_full_data/student_2022.rds"))
 
+# Create subset dataset
 student_subset_2022 <- student_2022 %>%
   group_by(country_iso3c) %>%  # Group by country
   sample_n(size = 50)          # Randomly sample 50 students from each country
 
+# Save the dataset file
 save(student_subset_2022, file = "data/student_subset_2022.rda")
